d3f0e89e34c76d2749e849b8689a80341729a582
BNCOSTA21 <bncosta1230@gmail.com>
2025-09-18 14:21:47 -0300

    Voltar para o início do funil (tela de login)

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export interface UserData {
   validationHtmlUrl?: string;
 }
 function App() {
-  const [currentScreen, setCurrentScreen] = useState<ScreenType>('pix');
+  const [currentScreen, setCurrentScreen] = useState<ScreenType>('login');
   const [userData, setUserData] = useState<UserData | null>(null);
 
   const navigateTo = (screen: ScreenType) => {
